047c5ab0a9eaf09c9f564291b72cbd1745790325
Sabrina Markon <phpsitescripts@outlook.com>
2021-01-04 23:21:20 -0700

    Add header, nav, and footer from design.

--- footer.php
+++ footer.php
@@ -19,19 +19,45 @@
 <!-- /.container -->
 
 <!-- Footer -->
-<footer class="py-5 bg-dark">
+<section class="footer-area footer-dark">
   <div class="container">
-    <p class="m-0 text-center text-white">
-      &copy;2020 SadiesViralBannersPlus.com. All rights reserved.
-      <br />Made with <i class="fa fa-heart" aria-hidden="true" style="color:#be1931"></i> by <a href="http://sabrinamarkon.com" target="_blank">Sabrina Markon</a></p>
-    </p>
-  </div>
-  <!-- /.container -->
-</footer>
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <div class="footer-logo text-center">
+          <a class="mt-30" href="/"><img src="images/sadieslogoSM.png" alt="Logo" style="border: 5px solid #0067f4; border-radius: 3px;"></a>
+        </div> <!-- footer logo -->
+        <!--                     <ul class="social text-center mt-60">
+                        <li><a href="https://facebook.com/uideckHQ"><i class="lni lni-facebook-filled"></i></a></li>
+                        <li><a href="https://twitter.com/uideckHQ"><i class="lni lni-twitter-original"></i></a></li>
+                        <li><a href="https://instagram.com/uideckHQ"><i class="lni lni-instagram-original"></i></a></li>
+                        <li><a href="#"><i class="lni lni-linkedin-original"></i></a></li>
+                    </ul> -->
+        <!-- social -->
+        <div class="footer-support text-center">
+          <span class="mail"></span>
+        </div>
+        <div class="copyright text-center mt-35">
+          <p class="text">&copy;2020 <a href="https://sadiesviralbannersplus.com" target="_blank" rel="nofollow">SadiesViralBannersPlus.com</a><br />Made with &#10084; by <a rel="nofollow" href="https://sabrinamarkon.com" target="_blank">Sabrina Markon</a></p>
+        </div> <!--  copyright -->
+      </div>
+    </div> <!-- row -->
+  </div> <!-- container -->
+</section>
 
 <!-- Bootstrap core JavaScript -->
 <script src="js/jquery/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
 <script src="js/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="js/modernizr-3.7.1.min.js"></script>
+
+<!-- Isotope js -->
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+
+<!-- Scrolling Nav js -->
+<script src="js/jquery.easing.min.js"></script>
+<script src="js/scrolling-nav.js"></script>
 
 <!-- Custom JavaScript -->
 <script src="js/customjs.js"></script>
--- header.php
+++ header.php
@@ -1,135 +1,182 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html class="no-js" lang="en">
 
-  <head>
+<head>
 
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta name="description" content="<?php echo $metadescription ?>" />
-	<meta name="author" content="Sabrina Markon" />
-	<base href = "/" />
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo $metadescription ?>" />
+    <meta name="author" content="Sabrina Markon" />
+    <base href="/" />
 
-  <title><?php echo $metatitle ?></title>
+    <title><?php echo $metatitle ?></title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.png" type="image/png">
 
     <!-- Bootstrap core CSS -->
-	  <link href="js/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="js/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
+
+    <!-- Magnific Popup CSS -->
+    <!-- <link rel="stylesheet" href="css/magnific-popup.css"> -->
+        
+    <!-- Slick CSS -->
+    <!-- <link rel="stylesheet" href="css/slick.css"> -->
+        
+    <!-- Line Icons CSS -->
+    <!-- <link rel="stylesheet" href="css/LineIcons.css"> -->
+        
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
     <!-- Custom styles -->
     <link href="css/custom.css" rel="stylesheet" />
     <link href="css/imagemaker.css" rel="stylesheet">
-  </head>
+</head>
 
-  <body>
+<body>
+
+    <!-- Preloader -->
+    <!-- <div class="preloader">
+        <div class="loader">
+            <div class="ytp-spinner">
+                <div class="ytp-spinner-container">
+                    <div class="ytp-spinner-rotator">
+                        <div class="ytp-spinner-left">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                        <div class="ytp-spinner-right">
+                            <div class="ytp-spinner-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="/">Sadie's Viral Banners Plus</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
+    <section class="navbar-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg">
 
-			<?php
-			if ((isset($_SESSION['username'])) && (isset($_SESSION['password']))) {
+                        <a class="navbar-brand" href="#">
+                            <img src="images/sadieslogoSM.png" alt="Sadie's Viral Banners Plus Logo" style="border: 5px solid #fff; border-radius: 3px; width: 100px;">
+                        </a>
 
-        ?>
-        <li class="nav-item active">
-          <a class="nav-link" href="/members">Main
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/profile">Profile</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/banners">Your Banners</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/textads">Buy Text Ads</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/promotional">Promote</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/faq">FAQ</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/terms">Terms</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/logout">Logout</a>
-        </li>
-        <?php
-			}	else {
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTwo" aria-controls="navbarTwo" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="toggler-icon"></span>
+                            <span class="toggler-icon"></span>
+                            <span class="toggler-icon"></span>
+                        </button>
 
-        ?>
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Home
-            <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/login">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/register">Register</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/faq">FAQ</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/terms">Terms</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/contact">Contact</a>
-        </li>
-        <?php
-			}
-			?>
-					
-          </ul>
-        </div>
-      </div>
-    </nav>
+                        <?php
+                        if ((isset($_SESSION['username'])) && (isset($_SESSION['password']))) {
+                        ?>
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarTwo">
+                                <ul class="navbar-nav m-auto">
+                                    <li class="nav-item active"><a class="page-scroll" href="/members">Main</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/profile">Profile</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/banners">Your Banners</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/textads">Buy Text Ads</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/promotional">Earn Money</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/faq">FAQ</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/terms">Terms</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/contact">Contact</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="navbar-btn d-none d-sm-inline-block">
+                                <ul>
+                                    <li><a class="solid" href="/logout">Logout</a></li>
+                                </ul>
+                            </div>
+                        <?php
+                        } else {
+                        ?>
+                            <div class="collapse navbar-collapse sub-menu-bar" id="navbarTwo">
+                                <ul class="navbar-nav m-auto">
+                                    <li class="nav-item active"><a class="page-scroll" href="/">Home</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/ads">Banners</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/about">About</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/faq">FAQ</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/terms">Terms</a></li>
+                                    <li class="nav-item"><a class="page-scroll" href="/contact">Contact</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="navbar-btn d-none d-sm-inline-block">
+                                <ul>
+                                    <li><a class="solid" href="/login">Login</a></li>
+                                    <li><a class="solid" href="/register">Register</a></li>
+                                </ul>
+                            </div>
+                        <?php
+                        }
+                        ?>
+                    </nav> <!-- navbar -->
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </section>
 
     <!-- Header with Background Image -->
     <?php
     $urlfile = basename($_SERVER['REQUEST_URI']);
     if ($urlfile === '/' || $urlfile === '') {
-      ?>
-      <header class="ja-headerimage">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <h1 class="title display-3 text-center text-white mt-4">Sadie's Viral Banners Plus</h1>
-            </div>
-          </div>
-        </div>
-      </header>
-      <?php
+    ?>
+        <section id="home" class="slider_area">
+            <div id="carouselThree" class="carousel slide" data-interval="false" data-ride="carousel">
+                <!--             <ol class="carousel-indicators">
+                <li data-target="#carouselThree" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselThree" data-slide-to="1"></li>
+                <li data-target="#carouselThree" data-slide-to="2"></li>
+            </ol> -->
+
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="slider-content">
+                                        <h1 class="title">Sadie's Viral Banners Plus</h1>
+                                        <p class="text">Sadies Viral Banners Jetting Your's Around The Globe Free!</p>
+                                        <ul class="slider-btn rounded-buttons">
+                                            <li><a class="main-btn rounded-one" href="#">GET STARTED FREE!</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div> <!-- row -->
+                        </div> <!-- container -->
+                        <div class="slider-image-box d-none d-lg-flex align-items-center">
+                            <div class="slider-image">
+                                <img src="images/globeandairplaneandstars.png" alt="Sadie's Viral Banners Plus! Hero Image">
+                            </div> <!-- slider-imgae -->
+                        </div> <!-- slider-imgae box -->
+                    </div> <!-- carousel-item -->
+                </div>
+        </section>
+    <?php
     } else {
-      ?>
-      <div class="ja-toppadding2"></div>
-      <?php
+    ?>
+        <div class="ja-toppadding2"></div>
+    <?php
     }
     ?>
 
     <!-- Page Content -->
     <div class="container">
 
-		<div class="row">
-			<div class="col-sm-12">		
+        <div class="row">
+            <div class="col-sm-12">
 
-<!-- CENTRAL PAGE CONTENT APPEARS BELOW HERE -->		
+                <!-- CENTRAL PAGE CONTENT APPEARS BELOW HERE -->
