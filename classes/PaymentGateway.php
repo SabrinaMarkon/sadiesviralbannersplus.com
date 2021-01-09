79c4af17b31f0fb7e5980854ae12bb5ea6800873
Sabrina Markon <phpsitescripts@outlook.com>
2021-01-08 17:34:58 -0700

    Abstract class for payment gateways.

---
+++ classes/PaymentGateway.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL);
+/**
+Payment gateway abstract class.
+PHP 7.4+
+@author Sabrina Markon
+@copyright 2020 Sabrina Markon, PHPSiteScripts.com
+@license LICENSE.md
+ **/
+// if (count(get_included_files()) === 1) { exit('Direct Access is not Permitted'); }
+# Prevent direct access to this file. Show browser's default 404 error instead.
+if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit;
+}
+
+abstract class PaymentGateway {
+    abstract protected function _payButton() :string;
+    abstract protected function _ipn() :void;
+}
