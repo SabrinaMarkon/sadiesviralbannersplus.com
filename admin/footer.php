218ab631eb96e4678694401246252d3a456899fc
Sabrina Markon <phpsitescripts@outlook.com>
2021-01-12 17:06:09 -0700

    Comment out popper because of map error and not using popper (yet anyway).

--- admin/footer.php
+++ admin/footer.php
@@ -1,28 +1,28 @@
-
 <!-- CENTRAL PAGE CONTENT APPEARS ABOVE HERE -->
-		    </div>
-	    </div>
+</div>
+</div>
 
-    </div>
-    <!-- /.container -->
+</div>
+<!-- /.container -->
 
-    <!-- Footer -->
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">
-		&copy;2020 SadiesViralBannersPlus.com. All rights reserved. 
-		<br />Made with <i class="fa fa-heart" aria-hidden="true" style="color:#be1931"></i> by <a href="http://sabrinamarkon.com" target="_blank">Sabrina Markon</a></p>
-		</p>
-      </div>
-      <!-- /.container -->
-    </footer>
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">
+      &copy;2020 SadiesViralBannersPlus.com. All rights reserved.
+      <br />Made with <i class="fa fa-heart" aria-hidden="true" style="color:#be1931"></i> by <a href="http://sabrinamarkon.com" target="_blank">Sabrina Markon</a>
+    </p>
+    </p>
+  </div>
+  <!-- /.container -->
+</footer>
 
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
-    <script src="../js/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="../js/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Custom JavaScript -->
-    <script src="../js/customjs.js"></script>
+<!-- Custom JavaScript -->
+<script src="../js/customjs.js"></script>
 
-  </body>
+</body>
 
 </html>
--- footer.php
+++ footer.php
@@ -12,9 +12,9 @@
       <div class="col-lg-6">
         <div class="footer-logo text-center">
           <a class="mt-30" href="/">
-          <!-- <img src="images/sadieslogoSM.png" alt="Logo" style="border: 5px solid #0067f4; border-radius: 3px;"> -->
-          <span class="logo">Sadie's <i class="fas fa-star fa-xs"></i></span>
-        </a>
+            <!-- <img src="images/sadieslogoSM.png" alt="Logo" style="border: 5px solid #0067f4; border-radius: 3px;"> -->
+            <span class="logo">Sadie's <i class="fas fa-star fa-xs"></i></span>
+          </a>
         </div> <!-- footer logo -->
         <!--        <ul class="social text-center mt-60">
                         <li><a href="https://facebook.com/uideckHQ"><i class="lni lni-facebook-filled"></i></a></li>
@@ -35,8 +35,8 @@
 </section>
 
 <!-- Bootstrap core JavaScript -->
-<script src="js/jquery/jquery.min.js"></script>
-<script src="js/popper.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<!-- <script src="https://unpkg.com/@popperjs/core@2"></script> -->
 <script src="js/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <script src="js/modernizr-3.7.1.min.js"></script>
