2f4f449ca82a6c435edb00a3f2d494817a54304c
Sabrina Markon <phpsitescripts@outlook.com>
2021-01-28 23:09:56 -0700

    Add thank-you page for successful member upgrade purchases.

---
+++ thankyou.php
@@ -0,0 +1,10 @@
+<?php
+# Prevent direct access to this file. Show browser's default 404 error instead.
+if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit;
+}
+
+$showcontent = new PageContent();
+echo $showcontent->showPage('Thank You Page - New Member Signup');
+?>
