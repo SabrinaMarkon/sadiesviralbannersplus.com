ac3de88fc844cd8b5986ee5c54a436db6e7a25cb
Sabrina Markon <phpsitescripts@outlook.com>
2021-02-15 01:11:07 -0700

    Remove unnecessary x-ua-compatible meta element.

--- admin/header.php
+++ admin/header.php
@@ -4,8 +4,8 @@
 <head>
 
   <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> -->
   <meta name="description" content="<?php echo $metadescription ?>" />
   <meta name="author" content="Sabrina Markon" />
   <base href="/" />
