802c44e806d179c400fad8a8ddc0405f5efdf522
Sabrina Markon <phpsitescripts@outlook.com>
2021-02-20 01:51:10 -0700

    Create main banners affiliate page.

---
+++ banners.php
@@ -0,0 +1,9 @@
+<?php
+# Prevent direct access to this file. Show browser's default 404 error instead.
+if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
+    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+    exit;
+}
+
+
+?>
