4d8786e94d24b4e9ddc571ea5236e197ea9f194b
Sabrina Markon <phpsitescripts@outlook.com>
2021-02-26 04:19:38 -0700

    Inject settings array into SendEmails method instead of individual settings.

--- admin/commands/SendEmails.php
+++ admin/commands/SendEmails.php
@@ -6,10 +6,8 @@
  * @copyright 2018 Sabrina Markon, PHPSiteScripts.com
  * @license LICENSE.md
  *
- * @param $domain The main url of the website.
- * @param $sitename The name of the website
- * @param $adminemail The website admin's support email address.
- * @param @adminname The name of the website admin.
+ * @param $settings Admin settings for the website.
+ *
  */
 require_once('../../config/Database.php');
 require_once('../../config/Settings.php');
@@ -26,7 +24,7 @@ class SendEmails
 
     private function __construct() {}
 
-    public static function getMails(string $domain, string $sitename, string $adminemail, string $adminname)
+    public static function getMails(array $settings): void
     {
         // get all mails that are marked as pending mailout.
         $pdo = Database::connect();
@@ -68,11 +66,11 @@ class SendEmails
 
                         // message disclaimer:
                         $disclaimer = "--------------------------------------------------------------<br><br>";
-                        $disclaimer .= "This is a message from ".$sitename.". You are receiving this because you are a double opted-in member of " . $sitename . " with username " . $username . "<br><br>";
-                        $disclaimer .= "You can opt out of receiving all emails from this website by logging in and deleting your account here:<br><br><a href=\"" . $domain . "/login\">" . $domain . "/login</a><br><br>";
+                        $disclaimer .= "This is a message from ". $settings['sitename'] .". You are receiving this because you are a double opted-in member of " . $settings['sitename'] . " with username " . $username . "<br><br>";
+                        $disclaimer .= "You can opt out of receiving all emails from this website by logging in and deleting your account here:<br><br><a href=\"" . $settings['domain'] . "/login\">" . $settings['domain'] . "/login</a><br><br>";
                         $disclaimer .= "Kindly allow up to 24 hours to stop receiving mail once you delete your account.<br><br>";
-                        $disclaimer .= "Thank you,<br>" . $adminname . "<br>" . $sitename . "<br><br><br>";
-                        $disclaimer .= "Live Removal Assistance or Questions: <a href=\"mailto:" . $adminemail . "\">" . $adminemail ."</a><br><br>";
+                        $disclaimer .= "Thank you,<br>" . $settings['adminname'] . "<br>" . $settings['sitename'] . "<br><br><br>";
+                        $disclaimer .= "Live Removal Assistance or Questions: <a href=\"mailto:" . $settings['adminemail'] . "\">" . $settings['adminemail'] ."</a><br><br>";
                         $disclaimer .= "This email is sent in strict compliance with International spam laws.<br><br>";
 
                         // full message and subject with disclaimer as well as this member's substitution:
@@ -92,12 +90,12 @@ class SendEmails
                         $htmlheader = "Content-Type: text/html; charset=windows-1252\n";
 
                         $sendsiteemail = new Email();
-                        $sendsiteemail->sendEmail($email, $adminemail, $subject, $html, $sitename, $adminemail, $htmlheader);
+                        $sendsiteemail->sendEmail($email, $settings['adminemail'], $subject, $html, $settings['sitename'], $settings['adminemail'], $htmlheader);
                         
                         if ($senderuserid === 'admin') {
 
                             # if the admin sent it, send a copy to the default admin email account.
-                            $sendsiteemail->sendEmail($adminemail, $adminemail, $subject, $html, $sitename, $adminemail, $htmlheader);
+                            $sendsiteemail->sendEmail($settings['adminemail'], $adminemail, $subject, $html, $settings['sitename'], $settings['adminemail'], $htmlheader);
                         }
                     }
                 }
@@ -123,9 +121,6 @@ class SendEmails
         Database::disconnect();
     }
 
-
-
-
 }
 
 $sitesettings = new Settings();
@@ -135,4 +130,4 @@ foreach ($settings as $key => $value)
     $$key = $value;
 }
 
-$mail = SendEmails::getMails($domain, $sitename, $adminemail, $adminname);
+$mail = SendEmails::getMails($settings);
