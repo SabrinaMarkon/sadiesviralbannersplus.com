0c662eec9df52e8347636781766bb254cdf2b8db
Sabrina Markon <phpsitescripts@outlook.com>
2021-03-23 01:30:24 -0600

    Add referral url to members viral banner page on promotional page in members area.

--- promotional.php
+++ promotional.php
@@ -6,9 +6,8 @@ if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
 }
 
 require "control.php";
-if (isset($show))
-{
-echo $show;
+if (isset($show)) {
+    echo $show;
 }
 $showcontent = new PageContent();
 echo $showcontent->showPage('Members Area Promotion Page');
@@ -20,99 +19,103 @@ $promotionals = $allpromotionals->getAllPromotionals();
 
 <div class="container">
 
-<h1 class="ja-bottompadding mb-3">Promotional Material</h1>
+    <h1 class="ja-bottompadding mb-4">Promotional Material</h1>
 
-<?php
-foreach ($promotionals as $promotional) {
+    <div class="affiliateurl" style="font-weight: bold; text-align: center; font-size: 1.4rem;">
+        Show off Your Best Programs on your Own Special VIRAL Banner App! <br /><a href="<?php echo $domain . "/banners/" . $username; ?>" target="_blank"><?php echo $domain . "/banners/" . $username; ?></a><br /><br />
+    </div>
 
-    if ($promotional['type'] == "banner") {
+    <?php
+    foreach ($promotionals as $promotional) {
+
+        if ($promotional['type'] == "banner") {
+
+    ?>
+            <div class="table-responsive border border-dark mb-5">
+
+                <table id="admintable" class="table table-condensed table-bordered table-striped text-center" style="margin-bottom: 0;">
+                    <tbody>
+                        <tr class="admintablebluebg">
+                            <td>
+                                <h4 class="ja-white">BANNER: <?php echo $promotional['name']; ?></h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="ja-promotionalimg">
+                                    <a href="<?php echo $domain ?>/r/<?php echo $username ?>" target="_blank"><img class="ja-promotionalimg" src="<?php echo $promotional['promotionalimage']; ?>"></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="referralurl">Referral URL:</label>
+                                <input type="text" name="referralurl" value="<?php echo $domain ?>/r/<?php echo $username ?>" class="form-control input-lg" size="40" readonly>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="promotionalimage">Banner Image URL:</label>
+                                <input type="text" name="promotionalimage" value="<?php echo $promotional['promotionalimage']; ?>" class="form-control input-lg" size="40" readonly>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="bannercode" class="control-label">Banner Code:&nbsp;</label>
+                                <textarea id="bannercode" class="form-control ja-promotionaltext" rows="2" readonly><a href="<?php echo $domain ?>/r/<?php echo $username ?>" target="_blank"><img src="<?php echo $promotional['promotionalimage']; ?>"></a></textarea>
+                                <button class="form-control btn btn-primary mt-2 w-25" onClick="copyToClipboard(document.getElementById('bannercode').value);return false;">COPY</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        <?php
+        }
+        if ($promotional['type'] == "email") {
 
         ?>
-        <div class="table-responsive border border-dark mb-5">
-            
-            <table id="admintable" class="table table-condensed table-bordered table-striped text-center" style="margin-bottom: 0;">
-                <tbody>
-                    <tr class="admintablebluebg">
-                        <td>
-                            <h4 class="ja-white">BANNER: <?php echo $promotional['name']; ?></h4>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="ja-promotionalimg">
-                                <a href="<?php echo $domain ?>/r/<?php echo $username ?>" target="_blank"><img class="ja-promotionalimg" src="<?php echo $promotional['promotionalimage']; ?>"></a>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="referralurl">Referral URL:</label>
-                            <input type="text" name="referralurl" value="<?php echo $domain ?>/r/<?php echo $username ?>" class="form-control input-lg" size="40" readonly>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="promotionalimage">Banner Image URL:</label>
-                            <input type="text" name="promotionalimage" value="<?php echo $promotional['promotionalimage']; ?>" class="form-control input-lg" size="40" readonly>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="bannercode" class="control-label">Banner Code:&nbsp;</label>
-                            <textarea id="bannercode" class="form-control ja-promotionaltext" rows="2" readonly><a href="<?php echo $domain ?>/r/<?php echo $username ?>" target="_blank"><img src="<?php echo $promotional['promotionalimage']; ?>"></a></textarea>
-                            <button class="form-control btn btn-primary mt-2 w-25" onClick="copyToClipboard(document.getElementById('bannercode').value);return false;">COPY</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <?php
+            <div class="table-responsive border border-dark mb-5">
+
+                <table id="admintable" class="table table-condensed table-bordered table-striped text-center" style="margin-bottom: 0;">
+                    <tbody>
+                        <tr class="admintablebluebg">
+                            <td>
+                                <h4 class="ja-white">EMAIL: <?php echo $promotional['name']; ?></h4>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div style="height: 300px; padding: 4px; overflow:auto; background: #ffffff;"><?php echo $promotional['promotionaladbody']; ?></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="referralurl">Referral URL:</label>
+                                <input type="text" name="referralurl" value="<?php echo $domain ?>/r/<?php echo $username ?>" class="form-control input-lg" size="40" readonly>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="promotionalsubject">Subject:</label>
+                                <input type="text" name="promotionalsubject" value="<?php echo $promotional['promotionalsubject']; ?>" class="form-control input-lg" size="40" readonly>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="emailcode">Message Text or HTML Code:</label>
+                                <textarea name="emailcode" class="form-control ja-promotionaltext" id="emailcode" rows="10" readonly><?php echo $promotional['promotionaladbody']; ?></textarea>
+                                <button class="form-control btn btn-primary mt-2 w-25" onClick="copyToClipboard(document.getElementById('emailcode').value);return false;">COPY</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    <?php
+
+        }
     }
-    if ($promotional['type'] == "email") {
+    ?>
 
-        ?>
-        <div class="table-responsive border border-dark mb-5">
-
-            <table id="admintable" class="table table-condensed table-bordered table-striped text-center" style="margin-bottom: 0;">
-                <tbody>
-                    <tr class="admintablebluebg">
-                        <td>
-                            <h4 class="ja-white">EMAIL: <?php echo $promotional['name']; ?></h4>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div style="height: 300px; padding: 4px; overflow:auto; background: #ffffff;"><?php echo $promotional['promotionaladbody']; ?></div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="referralurl">Referral URL:</label>
-                            <input type="text" name="referralurl" value="<?php echo $domain ?>/r/<?php echo $username ?>" class="form-control input-lg" size="40" readonly>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="promotionalsubject">Subject:</label>
-                            <input type="text" name="promotionalsubject" value="<?php echo $promotional['promotionalsubject']; ?>" class="form-control input-lg" size="40" readonly>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="emailcode">Message Text or HTML Code:</label>
-                            <textarea name="emailcode" class="form-control ja-promotionaltext" id="emailcode" rows="10" readonly><?php echo $promotional['promotionaladbody']; ?></textarea>
-                            <button class="form-control btn btn-primary mt-2 w-25" onClick="copyToClipboard(document.getElementById('emailcode').value);return false;">COPY</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <?php
-        
-    }
-}
-?>
-
-<div class="ja-bottompadding"></div>
+    <div class="ja-bottompadding"></div>
 
 </div>
