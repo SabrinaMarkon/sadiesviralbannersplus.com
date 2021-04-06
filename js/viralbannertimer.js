246afb9095ffe151beac7dd1b6728d3b6ed41809
Sabrina Markon <phpsitescripts@outlook.com>
2021-04-06 01:01:30 -0600

    Change RETURN link in Viral Banner timer bar to use history() to go back a page since we don't know whether the user was coming from /banners or /registration.

--- js/viralbannertimer.js
+++ js/viralbannertimer.js
@@ -5,7 +5,7 @@ function countdown(counter, id) {
 
     async function countDown() {
         if (counter > 0) {
-            document.getElementById("timerbar").innerHTML = '<img src="images/sadie-expression-' + whichsadie + '.png"><span class="sadietalknormal"><span class="sadietalk-blue"><strong>Hi there!</strong></span> Please check out the <strong>exciting opportunity</strong> below for <strong>&nbsp;<span class="sadietalk-pink">' + counter + '<strong></span>&nbsp;<span class="sadietalk-blue"><strong> seconds!<strong></span></span>';
+            document.getElementById("timerbar").innerHTML = '<img src="images/sadie-expression-' + whichsadie + '.png"><span class="sadietalknormal"><span class="sadietalk-blue"><strong>Hi there!</strong></span> Please check out the <strong>exciting opportunity</strong> below for <strong><span class="sadietalk-pink">' + counter + '<strong></span>&nbsp;<span class="sadietalk-blue"><strong> seconds!<strong></span></span>';
             counter--;
         } else {
             clearInterval(timer);
@@ -30,7 +30,7 @@ function countdown(counter, id) {
             // 6) After signup, create username record in viralbannerclicks table with their local storage array (JSON.parse again).
             // 7) Remove key from localStorage (in case someone else uses the same computer and wants to sign up!)
 
-            document.getElementById("timerbar").innerHTML = '<img src="images/sadie-expression-' + whichsadie + '.png"><span class="sadietalknormal"><strong><span class="sadietalk-blue">DONE!</span></strong>&nbsp;Your visit was <strong>counted!</strong> I hope you browse more! But you can <strong><span><a href="/banners">RETURN</a></strong> now if you like!</span></span>'; // ANNOUNCE HOW MANY MORE BANNERS THEY NEED TO CLICK! If done, say so!
+            document.getElementById("timerbar").innerHTML = '<img src="images/sadie-expression-' + whichsadie + '.png"><span class="sadietalknormal"><strong><span class="sadietalk-blue">DONE!</span></strong>&nbsp;Your visit was <strong>counted!</strong> I hope you browse more! But you can <strong><span><a href="javascript:window.history.back();">RETURN</a></strong> now if you like!</span></span>'; // ANNOUNCE HOW MANY MORE BANNERS THEY NEED TO CLICK! If done, say so!
         }
     }
 
